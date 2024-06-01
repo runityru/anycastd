eaa5b9a67bbd23b03479cf8626c6a739fa0c30a0
Igor Shishkin <me@teran.dev>
2024-06-01 19:19:39 +0300

    Add checkers logging and initialization mechanism (#4)
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- checkers/checkers.go
+++ checkers/checkers.go
@@ -1,7 +1,33 @@
 package checkers
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
 
 type Checker interface {
 	Check(ctx context.Context) error
+}
+
+var registry = map[string]func(in json.RawMessage) (Checker, error){}
+
+func NewCheckerByKind(kind string, spec json.RawMessage) (Checker, error) {
+	c, ok := registry[kind]
+	if !ok {
+		return nil, errors.Errorf("checker with kind `%s` is not registered", kind)
+	}
+
+	return c(spec)
+}
+
+func Register(kind string, fn func(in json.RawMessage) (Checker, error)) error {
+	if _, ok := registry[kind]; ok {
+		return errors.Errorf("checker with kind `%s` already registered", kind)
+	}
+
+	registry[kind] = fn
+
+	return nil
 }
--- checkers/http_2xx/spec.go
+++ checkers/http_2xx/spec.go
@@ -4,11 +4,11 @@ import (
 	"github.com/teran/anycastd/config"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 type spec struct {
-	Address  string          `json:"address"`
-	Path     string          `json:"path"`
+	URL      string          `json:"url"`
 	Method   string          `json:"method"`
 	Tries    uint8           `json:"tries"`
 	Interval config.Duration `json:"interval"`
@@ -17,8 +17,7 @@ type spec struct {
 
 func (s *spec) Validate() error {
 	return validation.ValidateStruct(s,
-		validation.Field(&s.Address, validation.Required),
-		validation.Field(&s.Path, validation.Required),
+		validation.Field(&s.URL, validation.Required, is.URL),
 		validation.Field(&s.Method, validation.Required),
 		validation.Field(&s.Tries, validation.Required),
 		validation.Field(&s.Interval, validation.Required),

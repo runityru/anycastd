c2d8f44425907f84fcacc44fa03673cec0eb3ffc
Igor Shishkin <me@teran.dev>
2024-06-02 10:51:41 +0300

    Add main
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- cmd/anycastd/main.go
+++ cmd/anycastd/main.go
@@ -1,5 +1,140 @@
-package anycastd
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	apipb "github.com/osrg/gobgp/v3/api"
+	"github.com/osrg/gobgp/v3/pkg/server"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/teran/anycastd/announcer"
+	"github.com/teran/anycastd/checkers"
+	"github.com/teran/anycastd/config"
+	"github.com/teran/anycastd/service"
+
+	// checkers in build
+	_ "github.com/teran/anycastd/checkers/http_2xx"
+)
+
+var (
+	appVersion     = "n/a (dev build)"
+	buildTimestamp = "undefined"
+)
+
+type spec struct {
+	ConfigPath string    `envconfig:"CONFIG_PATH" default:"/config.yaml"`
+	LogLevel   log.Level `envconfig:"LOG_LEVEL" default:"WARN"`
+}
 
 func main() {
-	panic("not implemented")
+	ctx := context.TODO()
+
+	s := spec{}
+	envconfig.MustProcess("", &s)
+
+	cfg, err := config.NewFromFile(s.ConfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetLevel(s.LogLevel)
+
+	lf := new(log.TextFormatter)
+	lf.FullTimestamp = true
+	log.SetFormatter(lf)
+
+	log.Infof("Initializing anycastd (%s @ %s) ...", appVersion, buildTimestamp)
+
+	g, _ := errgroup.WithContext(ctx)
+
+	log.Trace("Initializing BGP server ...")
+	bgpSrv := server.NewBgpServer(server.LoggerOption(&announcer.Logger{Logger: &log.Logger{
+		Out:       os.Stderr,
+		Formatter: lf,
+		Hooks:     make(log.LevelHooks),
+		Level:     s.LogLevel,
+	}}))
+
+	g.Go(func() error {
+		log.Trace("Starting to serve GoBGP API")
+		bgpSrv.Serve()
+		return nil
+	})
+
+	log.Trace("Starting BGP sessions ...")
+	if err := bgpSrv.StartBgp(ctx, &apipb.StartBgpRequest{
+		Global: &apipb.Global{
+			RouterId:   cfg.Announcer.RouterID,
+			Asn:        cfg.Announcer.LocalAS,
+			ListenPort: -1,
+		},
+	}); err != nil {
+		panic(err)
+	}
+	defer bgpSrv.StopBgp(ctx, &apipb.StopBgpRequest{})
+
+	if err := bgpSrv.WatchEvent(context.Background(), &apipb.WatchEventRequest{
+		Peer: &apipb.WatchEventRequest_Peer{},
+	}, func(r *apipb.WatchEventResponse) {
+		if p := r.GetPeer(); p != nil && p.Type == apipb.WatchEventResponse_PeerEvent_STATE {
+			log.Info(p)
+		}
+	}); err != nil {
+		panic(err)
+	}
+
+	for _, peer := range cfg.Announcer.Peers {
+		err = bgpSrv.AddPeer(context.Background(), &apipb.AddPeerRequest{
+			Peer: &apipb.Peer{
+				Conf: &apipb.PeerConf{
+					NeighborAddress: peer.RemoteAddress,
+					PeerAsn:         peer.RemoteAS,
+				},
+			},
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	log.Info("Starting service initialization ...")
+	for _, svcCfg := range cfg.Services {
+		log.Tracef("Initializing service %s ...", svcCfg.Name)
+
+		checks := []checkers.Checker{}
+		for _, check := range svcCfg.Checks {
+			log.WithFields(log.Fields{
+				"service": svcCfg.Name,
+				"check":   check.Kind,
+			}).Trace("registering check ...")
+
+			c, err := checkers.NewCheckerByKind(check.Kind, check.Spec)
+			if err != nil {
+				panic(err)
+			}
+
+			checks = append(checks, c)
+		}
+
+		a := announcer.New(announcer.Config{
+			GoBGP:    bgpSrv,
+			Prefixes: cfg.Announcer.Routes,
+			NextHop:  cfg.Announcer.LocalAddress,
+		})
+		svc := service.New(a, checks, time.Duration(svcCfg.CheckInterval))
+
+		g.Go(func() error {
+			return svc.Run(ctx)
+		})
+	}
+
+	log.Infof("Initialization completed")
+
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.22.3
 
 require (
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/osrg/gobgp/v3 v3.26.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.9.0
+	golang.org/x/sync v0.1.0
 	google.golang.org/protobuf v1.34.1
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -15,12 +17,31 @@ require (
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
+	github.com/eapache/channels v1.1.0 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/k-sone/critbitgo v1.4.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.16.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/vishvananda/netlink v1.2.1-beta.2 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 	google.golang.org/grpc v1.56.3 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 )

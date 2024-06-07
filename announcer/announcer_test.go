e28937f05acfb80f9c62b1cdc3e167a5d3bc12cd
Igor Shishkin <me@teran.dev>
2024-06-08 01:19:57 +0300

    Fix flaky platform dependent test (#39)
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- announcer/announcer_test.go
+++ announcer/announcer_test.go
@@ -2,22 +2,33 @@ package announcer
 
 import (
 	"context"
+	"regexp"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+)
+
+var reProtoString = regexp.MustCompile(
+	`^\[type\.googleapis\.com\/apipb\.IPAddressPrefix\]:\{prefix_len:32\s{1,2}prefix:\"172.16.38.43\"\}$`,
 )
 
 func TestAnnouncer(t *testing.T) {
 	r := require.New(t)
 
 	goBgpM := newGoBGPMock()
+
+	matcher := func(in string) bool {
+		return reProtoString.MatchString(in)
+	}
+
 	call1 := goBgpM.On(
 		"AddPath",
-		"[type.googleapis.com/apipb.IPAddressPrefix]:{prefix_len:32  prefix:\"172.16.38.43\"}",
+		mock.MatchedBy(matcher),
 	).Return([]byte("123456"), nil).Once()
 	goBgpM.On(
 		"DeletePath",
-		"[type.googleapis.com/apipb.IPAddressPrefix]:{prefix_len:32  prefix:\"172.16.38.43\"}",
+		mock.MatchedBy(matcher),
 	).Return(nil).NotBefore(call1).Once()
 
 	a := New(Config{
